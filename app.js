8b191198fb2fe7ea7c081d286d1b55fa3a9692bb
lakshmanakula1 <20MH1A0404@acoe.edu.in>
2025-02-05 19:56:54 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// async function fdata() {
+//     let apiData = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+//     console.log(apiData);
+//      let {categories} = await apiData.json()
+//     console.log(categories);
+    
+    
+//     let cont = document.createElement('div')
+//     categories.map(items =>{
+//         cont.innerHTML += `
+//         <h1>${items.strCategory}</h1>
+//         <img src =${items.strCategoryThumb}>
+//         `
+//     })
+//     document.body.append (cont)
+// }
+// fdata() 
