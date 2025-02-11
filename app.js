96178795e78b387af8ba27ff4b986ee4713a0d89
lakshmanakula1 <20MH1A0404@acoe.edu.in>
2025-02-11 11:50:13 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-// async function fdata() {
-//     let apiData = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-//     console.log(apiData);
-//      let {categories} = await apiData.json()
-//     console.log(categories);
-    
-    
-//     let cont = document.createElement('div')
-//     categories.map(items =>{
-//         cont.innerHTML += `
-//         <h1>${items.strCategory}</h1>
-//         <img src =${items.strCategoryThumb}>
-//         `
-//     })
-//     document.body.append (cont)
-// }
-// fdata() 
 function toggleMenu() {
     document.getElementById("sidebar").classList.toggle("active");
 }
+async function fdata() {
+    let apiData = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    console.log(apiData);
+     let {categories} = await apiData.json()
+    console.log(categories);
+    
+    
+    let cont = document.getElementById('category')
+    categories.map(items =>{
+        cont.innerHTML += `
+        <div id="cards">
+        <img src =${items.strCategoryThumb}>
+        </div>
+        
+        `
+    })
+    document.body.append (cont)
+}
+fdata() 
